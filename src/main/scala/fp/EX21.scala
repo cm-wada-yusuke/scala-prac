9b6754caacc76c28a4980624c6ea6eb084b42b0a
y-wada <yu.wasama00910706@gmail.com>
2016-03-21 01:40:52 +0900

    Exercise2.1を書いた。

---
+++ src/main/scala/fp/EX21.scala
@@ -0,0 +1,39 @@
+package fp
+
+/**
+  * Created by y-wada on 2016/03/20.
+  */
+object EX21 {
+
+  /**
+    * 美しい…。
+    * 加速度的に値が増加することを考えると、本当はIntを使うべきではない。
+    *
+    * @param n
+    * @return
+    */
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(t: Int, cur: Int, next: Int): Int =
+      if (t <= 1) cur
+      else if (t == 0) throw new RuntimeException("cant take zero value.")
+      else go(t - 1, next, cur + next)
+    go(n, 0, 1)
+  }
+
+  /**
+    * リストで並べてみたかった。最初、List.tabulate()で実装しようとしたがどうしてもzeroが排除できず断念。
+    *
+    * @param n
+    * @return
+    */
+  def fibs(n: Int): List[Int] = {
+    List.range(1, n).map(fib)
+  }
+
+  def main(args: Array[String]) {
+    println(fib(5))
+    println(fibs(10))
+  }
+
+}
