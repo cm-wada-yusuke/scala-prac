954de23d3027fc2b523bb06c527dde9d245cfd16
y-wada <yu.wasama00910706@gmail.com>
2016-03-21 15:54:19 +0900

    エクササイズ2.2を解いた。

--- build.sbt
+++ build.sbt
@@ -2,6 +2,6 @@ name := "scala-practice"
 
 version := "1.0"
 
-scalaVersion := "2.11.7"
+scalaVersion := "2.11.8"
 
 scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked", "-Xlint")
---
+++ src/main/scala/fp/EX22.scala
@@ -0,0 +1,37 @@
+package fp
+
+/**
+  * Created by y-wada on 2016/03/20.
+  */
+object EX22 {
+
+  /**
+    *
+    * @param as      検査対象の配列。
+    * @param ordered 2値をとりソート条件を満たしているかどうかを返す関数。
+    * @tparam A 検査対象配列の要素型
+    * @return asがソート済かどうか
+    */
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    def loop(n: Int): Boolean =
+      if (n >= as.length - 1) ordered(as(n - 1), as(n)) // 終わり
+      else if (as.length <= 1) true //要素がひとつ以下のときは常にtrue
+      else if (ordered(as(n - 1), as(n))) loop(n + 1)
+      else false
+    loop(1)
+  }
+
+
+  /**
+    * 本当はテストを書くべき。
+    *
+    * @param args メイン関数引数。
+    */
+  def main(args: Array[String]): Unit = {
+    val ordered = { (x: Int, y: Int) => y >= x }
+    val array = Array(1, 2, 4, 3, 6, 7, 8, 9, 10)
+    println(array.mkString(", "))
+    println(isSorted(array, ordered))
+  }
+
+}
